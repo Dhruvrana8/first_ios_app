41e44f112dfaea332b1420c543581ce015a6215f
Dhruv Rana <dhruvrana4@gmail.com>
2024-02-04 13:22:20 +0530

    Initial Commit

---
+++ First App/First_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppApp.swift
+//  First App
+//
+//  Created by Dhruv Rana on 04/02/24.
+//
+
+import SwiftUI
+
+@main
+struct First_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
